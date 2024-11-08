357faf5ecf2c57f0d22dc3634713519516e371c4
Noor-Afida-Annha <noorafidaannha2@gmail.com>
2024-11-08 23:55:29 +0600

    first commit

---
+++ src/test/java/UserTestRunner.java
@@ -0,0 +1,43 @@
+import com.github.javafaker.Faker;
+import io.restassured.path.json.JsonPath;
+import org.apache.commons.configuration.ConfigurationException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class UserTestRunner extends SetUp {
+    @Test(priority = 1, enabled = true)
+    public void doLogin() throws IOException, ConfigurationException {
+        UserController userController = new UserController();
+        userController.doLogin("admin@roadtocareer.net","1234");
+    }
+
+    @Test(priority = 3, enabled = true)
+    public void searchUser() throws IOException {
+        UserController userController = new UserController();
+        JsonPath jsonPath = userController.searchUser(prop.getProperty("userId"));
+        String message = jsonPath.get("message");
+        Assert.assertTrue(message.contains("User found"));
+    }
+
+    @Test(priority = 2)
+    public void createUser() throws ConfigurationException, IOException {
+        Faker faker = new Faker();
+        UserController userController = new UserController();
+        UserModel model = new UserModel();
+        model.setName(faker.name().fullName());
+        model.setEmail(faker.internet().emailAddress().toLowerCase());
+        model.setPassword("P@ssword123");
+        String phoneNumber = "01502"+Utils.generateRandomId(100000,99999);
+        model.setPhone_number(phoneNumber);
+        model.setNid(String.valueOf(Utils.generateRandomId(100000000,999999999)));
+        model.setRole("Customer");
+        JsonPath jsonPath = userController.createUser(model);
+        int userId= jsonPath.get("user.id");
+        Utils.setEnvVar("userId", String.valueOf(userId));
+
+
+
+    }
+}
